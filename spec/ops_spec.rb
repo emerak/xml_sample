3635c22f3fcb945ae25d515cbf5e3c2177461e30
Alejandra Cernas <alejandra.cernas@tangosource.com>
2013-10-23 13:39:21 -0500

    Added let for url and xml varibles

--- spec/ops_spec.rb
+++ spec/ops_spec.rb
@@ -3,13 +3,17 @@ require 'Ops'
 
 describe Ops do
   include HTTParty
-  let!(:url){ Ops.new.return_ops_url(1) }
+  before(:each) do
+    Ops.new
+  end
+  let!(:url){ Ops.return_ops_url(1) }
+  let!(:xml){ ops.return_shoopingcart_xml(1)}
   it "Returns the canvas Url given a template id" do
     #HTTParty.should_receive(:post).with(xml: {key: 1}.to_xml).and_return 'http://localhost:3000/mistemplates/1'
     url.should eql('http://localhost:3000/mistemplates/1')
   end
-  it "Redirects to ops url to widget" do
-    
 
+  it 'Returns the xml for shopping car' do
+    xml.should eql('<xml>mixml></xml>')
   end
 end
