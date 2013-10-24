f7e3ff90507a1a7d471d85e0d0685e37d4813937
Alejandra Cernas <alejandra.cernas@tangosource.com>
2013-10-24 16:08:06 -0500

    Removed binding pry

--- lib/Ops.rb
+++ lib/Ops.rb
@@ -4,18 +4,18 @@ class Ops
   attr_accessor :duns,:shared_secrete,:user_agent,:buyer_cookie,:cost_center_number,:employ_email,
     :user_name,:template_external_number,:url_return,:order_number, :start_point
   def initialize
-    @duns = "12345"
-    @shared_secret = "98765"
-    @user_agent = "Inkd.com"
+    @duns = '12345'
+    @shared_secret = '98765'
+    @user_agent = 'Inkd.com'
     @buyer_cookie = 'user'
-    @cost_center_number = "12354"
-    @employ_email = "johndoe@inkd.com"
-    @user_name = "johndoe"
-    @template_external_number = "48098"
-    @url_return = "http://localhost:3000/templates"
-    @order_number = "78983"
-    @status = "create"
-    @start_point = ""
+    @cost_center_number = '12354'
+    @employ_email = 'johndoe@inkd.com'
+    @user_name = 'johndoe'
+    @template_external_number = '48098'
+    @url_return = 'http://localhost:3000/templates'
+    @order_number = '78983'
+    @status = 'create'
+    @start_point = ''
     create_new_order
   end
 
@@ -27,6 +27,7 @@ class Ops
   end
 
   def create_new_order
+    @extrinc = { 'CostCenter'=> @cost_center_number, 'UserEmail'=> @employ_email, 'UniqueName'=> @user_name, 'StartPoint'=> @start_point }
     xml = Builder::XmlMarkup.new
     xml.instruct!
     xml.declare! :DOCTYPE, :cXML, :SYSTEM, "http://xml.cXML.org/schemas/cXML/1.2.007/cXML.dtd"
@@ -53,12 +54,16 @@ class Ops
       xml.Request("deploymentMode" => "test") do
         xml.PunchOutSetupRequest("operation" => "#{@status}") do
           xml.BuyerCookie @buyer_cookie
-          xml.Extrinsic('name'=>'costcenter') @cost_center_number
-          xml.Extrinsic('name'=>'UserEmail')  @employ_email
-          xml.Extrinsic('name'=>'UniqueName') @user_name
-          xml.Extrinsic('name'=>'StartPoint') @start_point
-          xml.BrowserFormatPost do
-            xml.URL @user_return
+          @extrinc.each do |k,v|
+            xml.Extrinsic(v,'name'=> k)
+          end
+          xml.BrowserFormPost do
+            xml.URL @url_return
+          end
+          xml.SelectedItem do
+            xml.ItemID do
+              xml.SupplierPartID @template_external_number
+            end
           end
         end
       end
