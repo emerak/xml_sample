f0f763dbcd785251f2eac931e8d94039df598be6
Alejandra Cernas <alejandra.cernas@tangosource.com>
2013-10-24 09:46:48 -0500

    Added attr accessors

--- lib/Ops.rb
+++ lib/Ops.rb
@@ -1,20 +1,21 @@
 require 'active_support'
 require 'builder'
 class Ops
-  attr_accesor :duns,:shared_secrete,:user_agent,:buyer_cookie,:cost_center_number,:employ_email,
-    :user_name,:template_external_number,:url_return,:order_number
+  attr_accessor :duns,:shared_secrete,:user_agent,:buyer_cookie,:cost_center_number,:employ_email,
+    :user_name,:template_external_number,:url_return,:order_number, :start_point
   def initialize
     @duns = "12345"
     @shared_secret = "98765"
     @user_agent = "Inkd.com"
-    @buyer_cookie = Session['user']
+    @buyer_cookie = 'user'
     @cost_center_number = "12354"
     @employ_email = "johndoe@inkd.com"
-    @username = "johndoe"
+    @user_name = "johndoe"
     @template_external_number = "48098"
-    @urlreturn = "http://localhost:3000/templates"
+    @url_return = "http://localhost:3000/templates"
     @order_number = "78983"
     @status = "create"
+    @start_point = ""
     create_new_order
   end
 
@@ -28,10 +29,10 @@ class Ops
   def create_new_order
     xml = Builder::XmlMarkup.new
     xml.instruct!
-    xml.declare! :DOCTYPE, :cXML, SYSTEM, "http://xml.cXML.org/schemas/cXML/1.2.007/cXML.dtd"
+    xml.declare! :DOCTYPE, :cXML, :SYSTEM, "http://xml.cXML.org/schemas/cXML/1.2.007/cXML.dtd"
     xml.cxml("playloadID" => "", "timestamp" =>"optional", "version"=>"1.0","xml:lang" => "en") do
       xml.Header do
-        xml.from do
+        xml.From do
           xml.Credential("domain"=>"DUNS") do
             xml.Identity @duns
           end
@@ -49,16 +50,21 @@ class Ops
           xml.UserAgent @user_agent
         end
       end
-      xml.Request("deplymentMode" => "test") do
-        xml.PunchOutSetupRequest("operation" => :status) do
+      xml.Request("deploymentMode" => "test") do
+        xml.PunchOutSetupRequest("operation" => "#{@status}") do
           xml.BuyerCookie @buyer_cookie
-          xml.Extrinsic('name' => "#{@buyer_cookie}")
+          xml.Extrinsic('name'=>'costcenter') @cost_center_number
+          xml.Extrinsic('name'=>'UserEmail')  @employ_email
+          xml.Extrinsic('name'=>'UniqueName') @user_name
+          xml.Extrinsic('name'=>'StartPoint') @start_point
+          xml.BrowserFormatPost do
+            xml.URL @user_return
+          end
         end
       end
 
 
     end
-    puts xml
   end
 
   def return_shoopingcart_xml(ops_template_id)
