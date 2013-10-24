352635ace8b43ba4b2baeeca942564b68e6badd1
Alejandra Cernas <alejandra.cernas@tangosource.com>
2013-10-24 16:59:54 -0500

    Created given xml from ops

--- app/controllers/requestxmls_controller.rb
+++ app/controllers/requestxmls_controller.rb
@@ -1,7 +1,24 @@
 class RequestxmlsController < ApplicationController
+  require 'builder'
   def receive_xml
     respond_to do |format|
-      format.xml { render :xml => "<siteurl>""http://localhost:3000/mistemplates/1""</siteurl>"}
+      format.xml { render :xml => create_xml }
+    end
+  end
+
+  def create_xml
+    xml = Builder::XmlMarkup.new
+    xml.instruct!
+    xml.declare! :DOCTYPE, :cXML, :SYSTEM, "http://xml.cXML.org/schemas/cXML/1.2.007/cXML.dtd"
+    xml.cxml("xml:lang" => "en-US","playloadID" => "", "timestamp" =>"optional") do
+      xml.Response do
+        xml.Satus("code" => "200", "text" => "succes")
+        xml.PunchOutSetupResponse do
+          xml.StartPage do
+            xml.URL "http://localhost:3000/mistemplates/1"
+          end
+        end
+      end
     end
   end
 end
